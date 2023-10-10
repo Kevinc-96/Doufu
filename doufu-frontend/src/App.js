50252f28451d63a3c88917376fa6fd7cb5680489
Kevin Chang <kevinyuanderchang@gmail.com>
2023-10-10 16:47:21 -0700

    added images and inital routing code

--- doufu-frontend/src/App.js
+++ doufu-frontend/src/App.js
@@ -1,11 +1,38 @@
+import data from './data';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
-    <div>
-      <header className="App-header">
-        <a href="/">doufu</a>
-      </header>
-      <main>List Items Here</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header className="App-header">
+          <a href="/">doufu</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+          <h2>Featured Pastries</h2>
+          <div className="items">
+            {data.products.map((item) => (
+              <div className="item" key={item.slug}>
+                <a href={`/product/${item.slug}`}>
+                  <img src={item.image} alt={item.name}></img>
+                </a>
+                <div className="item-details">
+                  <a href={`/product/${item.slug}`}>
+                    <p>{item.name}</p>
+                  </a>
+                  <p>${item.price}</p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ doufu-frontend/src/data.js
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      name: 'Red Bean Bun',
+      slug: 'red-bean-bun',
+      category: 'buns',
+      image: '/images/redbun.jpg',
+      price: 5,
+      stock: 10,
+      rating: 4.2,
+      reviewCount: 5,
+      desc: 'Delicious bun with red bean filling',
+    },
+    {
+      name: 'Burnt Cheese Cakes',
+      slug: 'burnt-cheese-cakes',
+      category: 'cakes',
+      image: '/images/burntcake.jpg',
+      price: 8,
+      stock: 10,
+      rating: 4.5,
+      reviewCount: 9,
+      desc: 'Delicious cheese cake with a burnt finish on the top',
+    },
+    {
+      name: 'Pineapple Bun',
+      slug: 'pineapple-bun',
+      category: 'buns',
+      image: '/images/pineapplebun.jpg',
+      price: 5,
+      stock: 12,
+      rating: 4.5,
+      reviewCount: 15,
+      desc: 'Delicious bun with a pineapple like look on the top',
+    },
+    {
+      name: 'Green Bean Bun',
+      slug: 'green-bean-bun',
+      category: 'buns',
+      image: '/images/greenbun.jpeg',
+      price: 5,
+      stock: 10,
+      rating: 4.2,
+      reviewCount: 5,
+      desc: 'Delicious bun with green bean filling',
+    },
+  ],
+};
+
+export default data;
