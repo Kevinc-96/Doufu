00f5ca52e6292bca8622d21488c7518ed0fadbfe
Kevin Chang <kevinyuanderchang@gmail.com>
2023-10-09 21:25:28 -0700

    initial commit with front end react app only

---
+++ doufu-frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">doufu</a>
+      </header>
+      <main>List Items Here</main>
+    </div>
+  );
+}
+
+export default App;
